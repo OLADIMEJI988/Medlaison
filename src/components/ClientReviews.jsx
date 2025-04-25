d627b1761ba6e16b6562fd652d5aeb96362675ba
OLADIMEJI988 <oladimejimax49@gmail.com>
2025-04-25 23:11:59 +0100

    integrated client reviews

---
+++ src/components/ClientReviews.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import './ClientReviews.css'
+import juiceWrld from '../assets/juiceWrld.png';
+import doctor from '../assets/doctor.png';
+import blackWoman from '../assets/blackWoman.png';
+import manInPink from '../assets/manInPink.png';
+import wrldInHoodie from '../assets/wrldInHoodie.png';
+import wrldInWhite from '../assets/wrldInWhite.png';
+
+
+export default function () {
+  return (
+    <>
+        <div className='wrapper'>
+            <div className='item item1 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={juiceWrld} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Mario lekan Jr</p>
+                </div>
+                <p className='text-[#727272] text-xs'>The personalized health insights from Medlaison have been invaluable. I've learned so much about my health patterns and received actionable view</p>
+            </div>
+
+            <div className='item item2 px-[10px] py-3 text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={doctor} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>John M</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Medlaison’s health record keeps my medical info accessible anywhere. The personalized insights help me stay aligned with my wellness goals.</p>
+            </div>
+
+            <div className='item item3 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={blackWoman} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Mario lekan Jr</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Medlaison has been a game-changer for me. I can access all my medical records in one place, and the real-time consultations have saved me countless trips.</p>
+            </div>
+
+            <div className='item item4 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={manInPink} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Michael S</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Using Medlaison daily has simplified my health routine. I track progress, get expert advice, and access records anytime, anywhere with ease.</p>
+            </div>
+
+            <div className='item item5 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={wrldInHoodie} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Emily R</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Thanks to Medlaison, managing my health feels effortless. I monitor symptoms, consult professionals instantly, and stay informed with personalized updates.</p>
+            </div>
+
+            <div className='item item6 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={wrldInWhite} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Sarah D</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Medlaison gives me control over my well-being. I review my history, schedule quick consultations, and receive tips tailored to my health journey.</p>
+            </div>
+        </div>
+
+        <div className='wrapper'>
+            <div className='item item1 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={blackWoman} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Mario lekan Jr</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Medlaison has been a game-changer for me. I can access all my medical records in one place, and the real-time consultations have saved me countless trips.</p>
+            </div>
+
+            <div className='item item2 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={juiceWrld} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Mario lekan Jr</p>
+                </div>
+                <p className='text-[#727272] text-xs'>The personalized health insights from Medlaison have been invaluable. I've learned so much about my health patterns and received actionable view</p>
+            </div>
+
+            <div className='item item3 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={wrldInHoodie} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Emily R</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Thanks to Medlaison, managing my health feels effortless. I monitor symptoms, consult professionals instantly, and stay informed with personalized updates.</p>
+            </div>
+
+            <div className='item item4 px-[10px] py-3 text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={doctor} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>John M</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Medlaison’s health record keeps my medical info accessible anywhere. The personalized insights help me stay aligned with my wellness goals.</p>
+            </div>
+
+            <div className='item item5 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={wrldInWhite} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Sarah D</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Medlaison gives me control over my well-being. I review my history, schedule quick consultations, and receive tips tailored to my health journey.</p>
+            </div>
+
+            <div className='item item6 px-[10px] py-[10px] text-start shadow-lg shadow-[#CCCCCC40]'>
+                <div className='flex gap-[6px] mb-3'>
+                    <img className='h-9 rounded-[4px]' src={manInPink} alt="" />
+                    <p className='text-[#414141] text-[13px] font-semibold mt-4'>Michael S</p>
+                </div>
+                <p className='text-[#727272] text-xs'>Using Medlaison daily has simplified my health routine. I track progress, get expert advice, and access records anytime, anywhere with ease.</p>
+            </div>
+        </div>
+    </>
+
+  )
+}
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import ClientReviews from './ClientReviews'
 
 export default function Reviews() {
   return (
     <>
-        <div className='mt-20 mb-5 font-semibold'>
+        <div className='mt-20 mb-7 font-semibold'>
             <p className='text-[#1324BB] text-sm font-bold'>Reviews</p>
             <p className='text-[#171717] text-[25px] mt-[2px]'>Testimony from over 100k users</p>
        </div>
+       <ClientReviews />
     </>
   )
 }
